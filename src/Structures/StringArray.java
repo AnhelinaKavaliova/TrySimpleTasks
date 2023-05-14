de5ae9cfdad3a608bc63bf978efbf99afcdc3727
AnhelinaKavaliova <angelinakovaleva228@gmail.com>
2023-05-14 10:39:30 +0300

    Add task

---
+++ src/Structures/StringArray.java
@@ -0,0 +1,39 @@
+package Structures;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class StringArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of strings: ");
+        int stringNum = scanner.nextInt();
+        scanner.nextLine();
+
+        String[] s = new String[stringNum];
+
+        for(int i = 0; i < stringNum; i++){
+            int k = i + 1;
+            System.out.print("Enter " + k + " string: ");
+            s[i] = scanner.nextLine();
+        }
+        //Arrays.sort(s);
+        for(int i = 0; i < stringNum; i++){
+            for(int j = i+1; j < stringNum; j++){
+                if(s[i].length() >s [j].length()){
+                    String tmp = s[i];
+                    s[i] = s[j];
+                    s[j] = tmp;
+
+                }
+            }
+
+        }
+
+        for(String str : s){
+            System.out.println(str);
+        }
+
+
+    }
+}
